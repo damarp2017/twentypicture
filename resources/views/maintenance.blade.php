9b58525e5bcb6f2ea497ff40580a9ae44fc1aabf
Damar Permadany <damarp2017@gmail.com>
2021-05-11 21:42:56 +0700

    maintenance

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Opps sorry</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@900&display=swap" rel="stylesheet">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Source Sans Pro', sans-serif;
+        }
+    </style>
+</head>
+
+<body class="bg-pink-100 p-10 md:p-10 xl:p-20 flex flex-col space-y-5 h-screen">
+    <h1 class="text-gray-800 text-xl md:text-2xl lg:text-4xl">Sorry, website is under maintenance.</h1>
+    <h1 class="text-gray-800 text-xl md:text-2xl lg:text-4xl">We'll back soon.</h1>
+</body>
+
+</html>
